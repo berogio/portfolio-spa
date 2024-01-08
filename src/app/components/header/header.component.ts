3f4c33f771a2f75ce776e8810a73aa95da6da568
berogio <giorgi.beridze01e@geolab.edu.ge>
2024-01-08 21:07:11 +0100

    AddEdHeaderAndBurgerMenu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,3 @@
-// header.component.ts
-
 import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
@@ -11,8 +9,25 @@ import { filter } from 'rxjs/operators';
 })
 export class HeaderComponent implements AfterViewInit {
   @ViewChild('navbar', { static: true }) private navbar!: ElementRef;
+  isMenuOpen = false;
+  navLinks = [
+    { label: 'Skills', routerLink: '/skills', section: 'skills' },
+    { label: 'Experience', routerLink: '/experience', section: 'experience' },
+    { label: 'About Me', routerLink: '/about', section: 'about' },
+    { label: 'Contact Me', routerLink: '/contact', section: 'contact' },
+  ];
 
   constructor(private router: Router, private route: ActivatedRoute) {}
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+
+    // Hier wird die "open" Klasse basierend auf dem Menüstatus hinzugefügt oder entfernt
+    const burgerMenu = document.querySelector('.burger-menu');
+    if (burgerMenu) {
+      burgerMenu.classList.toggle('open', this.isMenuOpen);
+    }
+  }
 
   ngAfterViewInit() {
     this.router.events
