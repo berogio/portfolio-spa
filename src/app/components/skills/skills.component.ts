9c50e46856650f3d24f9deb034fce2fb976eaf12
berogio <giorgi.beridze01e@geolab.edu.ge>
2024-01-10 15:06:52 +0100

    AddedResposiveTextFilppCards

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  styleUrls: ['./skills.component.scss'],
 })
 export class SkillsComponent {
+  isRotated: boolean[] = Array(10).fill(false);
 
+  rotateDiv(index: number) {
+    this.isRotated[index] = !this.isRotated[index];
+  }
 }
