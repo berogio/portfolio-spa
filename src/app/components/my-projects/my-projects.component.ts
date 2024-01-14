f86c41be6de7c8549118169a3cff6df5f7efe28c
berogio <giorgi.beridze01e@geolab.edu.ge>
2024-01-14 21:32:08 +0100

    test2

--- src/app/components/my-projects/my-projects.component.ts
+++ src/app/components/my-projects/my-projects.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-my-projects',
   templateUrl: './my-projects.component.html',
-  styleUrls: ['./my-projects.component.scss']
+  styleUrls: ['./my-projects.component.scss'],
 })
-export class MyProjectsComponent {
-
-}
+export class MyProjectsComponent {}
