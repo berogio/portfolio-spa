5cae3045f14abecd2bf202b7c0029675a41102fa
berogio <giorgi.beridze01e@geolab.edu.ge>
2024-01-20 02:05:41 +0100

    AddedEducation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,9 @@
-// app.module.ts
-
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { HeaderComponent } from './components/header/header.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ExperienceComponent } from './components/experience/experience.component';
 import { AboutComponent } from './components/about/about.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { MyProjectsComponent } from './components/my-projects/my-projects.component';
 
 const routes: Routes = [
@@ -17,7 +12,6 @@ const routes: Routes = [
   { path: 'skills', component: SkillsComponent },
   { path: 'experience', component: ExperienceComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
   { path: 'projects', component: MyProjectsComponent },
 ];
 
--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,65 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+interface Card {
+  imageUrl: string;
+  altText: string;
+  text1: string;
+  text2: string;
+  text3: string;
+  text4?: string;
+  text5?: string;
+  website?: string;
+}
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent {
+  contactForm!: FormGroup;
+  cards: Card[] = [
+    {
+      imageUrl: '/assets/images/AgrUniLogo.png',
+      altText: 'Bildbeschreibung 1',
+      text1: 'Agricultural University',
+      text2: '🟢 - Automation Systems and Controls',
+      text3: '🟢 - Emerging Technologies Integration',
+      text4: '🟢 - Computer Engineering',
+      website: 'https://agruni.edu.ge',
+    },
+    {
+      imageUrl: '/assets/images/logo-bitcamp.jpg',
+      altText: 'Bildbeschreibung 2',
+      text1: 'Bootcamp',
+      text2: '🔴 - Front-End, Responsive Design ',
+      text3: '🔴 - Back-End API Communication',
+      text4: '🔴 - Servers and Databases',
+      website: '',
+    },
+    {
+      imageUrl: '/assets/images/TbcLogo.png',
+      altText: 'Bildbeschreibung 3',
+      text1: 'TBC x USAID ',
+      text2: '🔵 - Introduction to DevOps',
+      text3: '🔵 - Cloud Technologies',
+      text4: '🔵 - Infrastructure As Code (IaC)',
+      text5: 'notice: I`m currently in the learning phase',
+      website: 'https://www.tbcacademy.ge',
+    },
+  ];
 
+  constructor(private fb: FormBuilder) {
+    this.initForm();
+  }
+
+  private initForm() {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+    });
+  }
+
+  submitForm() {}
 }
