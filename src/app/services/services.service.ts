4bf21ec6f87233da57e1ef5770b8bb2ee9cc0997
berogio <giorgi.beridze01e@geolab.edu.ge>
2024-01-24 20:47:51 +0100

    quickFix

--- src/app/services/services.service.ts
+++ src/app/services/services.service.ts
@@ -14,7 +14,7 @@ import {
   providedIn: 'root',
 })
 export class ServicesService {
-  private baseUrl = 'http://18.153.93.163';
+  private baseUrl = 'https://portfolio-api-production-6224.up.railway.app';
   private endpoints: any = {
     experiences: '/experiences',
     projects: '/projects',
