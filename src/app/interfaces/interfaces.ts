e9b9dd7e602bcc26b7906e2b5208993621c294c4
berogio <giorgi.beridze01e@geolab.edu.ge>
2024-01-28 21:00:11 +0100

    changedInterface

--- src/app/interfaces/interfaces.ts
+++ src/app/interfaces/interfaces.ts
@@ -1,12 +1,19 @@
+export interface NavLink {
+  id: any;
+  language: string;
+  label: string;
+}
+
 export interface WorkExperience {
+  id: any;
   logo: string;
-  position: string;
   year: string;
+  position: string;
   description: string;
 }
 
 export interface Project {
-  id: number;
+  id: any;
   title: string;
   description: string;
   technologiesUsed: string[];
@@ -15,27 +22,32 @@ export interface Project {
 }
 
 export interface Card {
+  id: any;
   imageUrl: string;
   altText: string;
   organization: string;
   focusArea: string;
   additionalInfo1?: string;
   additionalInfo2?: string;
-  notice?: string;
   website?: string;
+  notice?: string;
+  language: string;
 }
 export interface Skills {
+  id: any;
   icon: string;
   name: string;
   description: string;
 }
 
 export interface ContactFormData {
+  id: any;
   email: string;
   message: string;
 }
 
 export interface Project {
+  id: any;
   title: string;
   description: string;
   description2: string;
@@ -44,15 +56,12 @@ export interface Project {
   liveDemoLink: string;
 }
 
-export interface NavLink {
-  label: string;
-}
-
 export interface WelcomeData {
+  id: any;
+  language: string;
+  greeting: string;
+  aboutHeader: string;
   aboutDescription: string;
   aboutFooter: string;
-  aboutHeader: string;
-  greeting: string;
   technologies: string[];
-  language: string;
 }
