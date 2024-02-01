784f3c13b0e7bd3f9bd986ee6ab416245a3341bd
berogio <giorgi.beridze01e@geolab.edu.ge>
2024-02-01 02:58:19 +0100

    test2

--- src/app/services/services.service.ts
+++ src/app/services/services.service.ts
@@ -14,7 +14,7 @@ import {
   providedIn: 'root',
 })
 export class ServicesService {
-  private baseUrl = 'http://localhost:3000';
+  private baseUrl = 'https://portfolio-api-production-6224.up.railway.app';
   private endpoints: any = {
     welcome: '/welcome',
     experiences: '/experiences',
